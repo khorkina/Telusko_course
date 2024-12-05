7ea2d3d08083b6d9c0c4007bc55b011c4b6250ff
SimpliLern <marinakhorkina2006@gmail.com>
2024-12-05 06:15:22 +0100

    Quize

---
+++ src/main/java/Quizlet/Main.java
@@ -0,0 +1,8 @@
+package Quizlet;
+
+public class Main {
+    public static void main(String[] args) {
+        QuestionService service = new QuestionService();
+        service.playQuiz();
+    }
+}
